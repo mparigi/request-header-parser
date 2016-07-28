bf2f5647da09d807f7936c47fa8f7bc8d48aba81
Makarand <mparigi99@gmail.com>
2016-07-28 18:12:51 +0000

    all done

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ var express = require("express");
 var app = express();
 
 app.get("/api/whoami", function (req, res) {
-    console.log(req);
+    var os = req.headers["user-agent"].split(') ')[0].split(' (')[1];
+    var ip = req.headers["x-forwarded-for"];
+    var lang = req.headers["accept-language"].split(',')[0];
+    
+    res.end(JSON.stringify({
+        "ipaddress": ip,
+        "language": lang,
+        "software": os
+    }));
 });
 
 app.listen(process.env.PORT || 8080)
